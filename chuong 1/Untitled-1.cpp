5ae6452d68f00cd9d5cf689358e596404737a9d0
HZhalex <97648418+HZhalex@users.noreply.github.com>
2025-03-05 09:53:51 +0700

    Add files via upload

---
+++ chuong 1/Untitled-1.cpp
@@ -0,0 +1,66 @@
+
+#include <iostream>
+using namespace std;
+/*
+//Bai 1
+int main(){
+    int a,b,c;
+    cout << "nhap vao 3 so nguyen a,b,c:";
+    cin >> a >> b >> c;
+    int tong, tich ;
+    tong = a +b+c;
+    tich = a*b*c;
+    cout << "\n tong cua 3 so a,b,c la:" << tong;
+    cout << "\n tich cua 3 so a,b,c la:" << tich; 
+    return 0;
+}
+*/
+/*
+//Bai 2
+int main(){
+    float a,b,chu_vi,dien_tich;
+    cout << "nhap vao 2 canh cua hinh chu nhat:";
+    cin >> a >> b;
+    chu_vi = 2* (a+b);
+    dien_tich = a*b;
+    cout << "\n chu vi cua hinh chu nhat la:" << chu_vi;
+    cout << "\n dien tich cua hinh chu nhat la:" << dien_tich;
+    return 0;
+}*/
+/*
+//Bai 3
+int main(){
+    float r,dien_tich,chu_vi;
+    cout << "nhap vao ban kinh hinh tron:";
+    cin >> r;
+    dien_tich = 3.14 * r*r;
+    chu_vi = 3.14 *2 * r;
+    cout << "\n chu vi cua hinh tron la:" << chu_vi;
+    cout << "\n dien tich cua hinh tron la:" << dien_tich; 
+    return 0;
+}
+*/
+/*
+//bai 4
+int main(){
+    int don_gia,so_luong,tien;
+    float thuegiatrigiatang;
+    cout << "so luong don hang la:";
+    cin >> so_luong;
+    cout << "don gia la:";
+    cin >> don_gia;
+    tien = so_luong * don_gia;
+    thuegiatrigiatang = tien * 10 / 100 ;
+    cout << "\nso tien la :" << tien;
+    cout << "\nthue gia tri gia tang la:" << thuegiatrigiatang;
+    
+}
+*/
+
+
+
+
+
+
+
+
